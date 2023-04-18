f78f39db220285e302dfd8111019c6c58159ab96
Matt Scafe <matt.scafe@rackspace.com>
2023-04-18 18:46:19 -0500

    Removed Provider block

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    vcd = {
-      source = "vmware/vcd"
-      version = "3.8.2"
-    }
-  }
-  required_version = "1.2.6"
-}
-
 # Create the Datacenter Group data source
 data "vcd_vdc_group" "dcgroup" {
   name = var.vdc_group_name
