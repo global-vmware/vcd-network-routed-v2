9dc8d02bef66c32e9b4f28cc278efdc897105a10
Matt Scafe <matt.scafe@rackspace.com>
2023-04-19 12:06:19 -0500

    Removed Some of the Default variable Values

--- variables.tf
+++ variables.tf
@@ -1,51 +1,59 @@
 variable "vdc_group_name" {
   description = "Cloud Director Datacenter Group Name"
+  type = string
+  default = ""
 }
 
 variable "vdc_org_name" {
   description = "Cloud Director Organization Name"
+  type = string
+  default = ""
 }
 
 variable "vdc_edge_name" {
   description = "Cloud Director Edge Name"
+  type = string
+  default = ""
 }
 
 variable "vcd_network_routed_prefix_length_24" {
   description = "CIDR notation prefix length for the 24-bit network"
+  type = number
   default = "24"
 }
 
 variable "vcd_network_routed_segment_segment_name" {
   description = "Name of the segment routed network"
-  default = "segment"
+  type = string
+  default = ""
 }
 
 variable "vcd_network_routed_segment_segment_gateway" {
   description = "Gateway IP address for the segment routed network"
-  default = "192.168.0.1"
+  default = ""
 }
 
 variable "vcd_network_routed_segment_start_address" {
   description = "Start IP address of the static IP pool for the segment network"
-  default = "192.168.0.51"
+  default = ""
 }
 
 variable "vcd_network_routed_segment_end_address" {
   description = "End IP address of the static IP pool for the segment network"
-  default = "192.168.0.100"
+  default = ""
 }
 
 variable "vcd_network_routed_segment_dns1_server_address" {
   description = "Primary DNS server address for the segment network"
-  default = "69.20.0.164"
+  default = ""
 }
 
 variable "vcd_network_routed_segment_dns2_server_address" {
   description = "Secondary DNS server address for the segment network"
-  default = "69.20.0.196"
+  default = ""
 }
 
 variable "vcd_network_routed_segment_dns_suffix" {
   description = "DNS suffix for the segment network"
-  default = "segment.local"
+  default = ""
 }
