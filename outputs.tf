0b97d8e82e55880f6a0bd4e24f1a612eb70ff581
Matt Scafe <matt.scafe@rackspace.com>
2023-04-19 17:54:24 -0500

    updated references from segment to org-vdc-routed-network

--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,31 @@
-output "segment_network_id" {
-  value = vcd_network_routed_v2.segment.id
+output "org-vdc-routed-network_network_id" {
+  value = vcd_network_routed_v2.org-vdc-routed-network.id
 }
 
-output "segment_network_name" {
-  value = vcd_network_routed_v2.segment.name
+output "org-vdc-routed-network_network_name" {
+  value = vcd_network_routed_v2.org-vdc-routed-network.name
 }
 
-output "segment_network_prefix_length" {
-  value = vcd_network_routed_v2.segment.prefix_length
+output "org-vdc-routed-network_network_prefix_length" {
+  value = vcd_network_routed_v2.org-vdc-routed-network.prefix_length
 }
 
-output "segment_network_dns1" {
-  value = vcd_network_routed_v2.segment.dns1
+output "org-vdc-routed-network_network_dns1" {
+  value = vcd_network_routed_v2.org-vdc-routed-network.dns1
 }
 
-output "segment_network_dns2" {
-  value = vcd_network_routed_v2.segment.dns2
+output "org-vdc-routed-network_network_dns2" {
+  value = vcd_network_routed_v2.org-vdc-routed-network.dns2
 }
 
-output "segment_network_dns_suffix" {
-  value = vcd_network_routed_v2.segment.dns_suffix
+output "org-vdc-routed-network_network_dns_suffix" {
+  value = vcd_network_routed_v2.org-vdc-routed-network.dns_suffix
 }
 
-output "segment_network_gateway" {
-  value = vcd_network_routed_v2.segment.gateway
+output "org-vdc-routed-network_network_gateway" {
+  value = vcd_network_routed_v2.org-vdc-routed-network.gateway
 }
 
-output "segment_network_static_ip_pool" {
-  value = vcd_network_routed_v2.segment.static_ip_pool
+output "org-vdc-routed-network_network_static_ip_pool" {
+  value = vcd_network_routed_v2.org-vdc-routed-network.static_ip_pool
 }
