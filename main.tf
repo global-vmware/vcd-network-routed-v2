177be6eb4d81cb3360e9baf849bdbc70806489f3
Matt Scafe <matt.scafe@rackspace.com>
2023-04-19 11:01:17 -0500

    Updated all references to segment01 and replaced with segment

--- main.tf
+++ main.tf
@@ -25,19 +25,19 @@ data "vcd_nsxt_edgegateway" "t1" {
 # Org VDC Routed Network
 ######################### 
 
-resource "vcd_network_routed_v2" "segment01" {
+resource "vcd_network_routed_v2" "segment" {
   org             = var.vdc_org_name
-  name            = var.vcd_network_routed_segment01_segment_name
+  name            = var.vcd_network_routed_segment_segment_name
   edge_gateway_id = data.vcd_nsxt_edgegateway.t1.id
 
-  gateway         = var.vcd_network_routed_segment01_segment_gateway
+  gateway         = var.vcd_network_routed_segment_segment_gateway
   prefix_length   = var.vcd_network_routed_prefix_length_24
-  dns1            = var.vcd_network_routed_segment01_dns1_server_address
-  dns2            = var.vcd_network_routed_segment01_dns2_server_address
-  dns_suffix      = var.vcd_network_routed_segment01_dns_suffix
+  dns1            = var.vcd_network_routed_segment_dns1_server_address
+  dns2            = var.vcd_network_routed_segment_dns2_server_address
+  dns_suffix      = var.vcd_network_routed_segment_dns_suffix
 
   static_ip_pool {
-    start_address = var.vcd_network_routed_segment01_start_address
-    end_address   = var.vcd_network_routed_segment01_end_address
+    start_address = var.vcd_network_routed_segment_start_address
+    end_address   = var.vcd_network_routed_segment_end_address
   }
 }
