fad2847df1c41363eaa0c427c8738d7c761d4213
Matt Scafe <matt.scafe@rackspace.com>
2023-04-19 21:42:03 -0500

    Shortened output names.

--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,27 @@
-output "org-vdc-routed-network_network_id" {
-  value = vcd_network_routed_v2.org-vdc-routed-network.id
-}
-
-output "org-vdc-routed-network_network_name" {
+output "network_name" {
   value = vcd_network_routed_v2.org-vdc-routed-network.name
 }
 
-output "org-vdc-routed-network_network_prefix_length" {
+output "network_prefix_length" {
   value = vcd_network_routed_v2.org-vdc-routed-network.prefix_length
 }
 
-output "org-vdc-routed-network_network_dns1" {
+output "network_dns1" {
   value = vcd_network_routed_v2.org-vdc-routed-network.dns1
 }
 
-output "org-vdc-routed-network_network_dns2" {
+output "network_dns2" {
   value = vcd_network_routed_v2.org-vdc-routed-network.dns2
 }
 
-output "org-vdc-routed-network_network_dns_suffix" {
+output "network_dns_suffix" {
   value = vcd_network_routed_v2.org-vdc-routed-network.dns_suffix
 }
 
-output "org-vdc-routed-network_network_gateway" {
+output "network_gateway" {
   value = vcd_network_routed_v2.org-vdc-routed-network.gateway
 }
 
-output "org-vdc-routed-network_network_static_ip_pool" {
+output "network_static_ip_pool" {
   value = vcd_network_routed_v2.org-vdc-routed-network.static_ip_pool
 }
